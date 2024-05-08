9c2dd8bb2e23b02ad18b8b91a7b51c2e9d384ae7
toonnaq <tooncopycat@gmail.com>
2024-05-09 00:42:23 +0700

    Add files via upload

---
+++ styles/ani/lightbox.js
@@ -0,0 +1,106 @@
+/*
+|-------------------------------------------
+|	Simple Lightbox Video jQuery
+|-------------------------------------------
+|
+|
+|	Author: Pedro Marcelo
+|	Author URI: https://github.com/pedromarcelojava
+|	Plugin URI: https://github.com/pedromarcelojava/Simple-Lightbox-Video-jQuery
+|	Version: 2.0
+*/
+
+(function($)
+{
+	$.extend($.fn, {
+		simpleLightboxVideo : function()
+		{
+			var defaults = {
+				delayAnimation: 0,
+				keyCodeClose: 27
+			};
+
+			$.simpleLightboxVideo.vars = $.extend({}, defaults);
+
+			var video = this;
+
+			video.click(function()
+			{
+				if (window.innerHeight > 540)
+				{
+					var margintop = (window.innerHeight - 540) / 2;
+				}
+				else
+				{
+					var margintop = 0;
+				}
+
+				var ifr = '<iframe src="" width="960" height="540" id="video-embed" style="border:0;" allowfullscreen></iframe>';
+				var lightbox = '<div class="lightbox" style="margin-top:' + margintop + 'px">';
+				var back = '<div id="back-lightbox">';
+				var bclo = '<div id="background-close"></div>';
+				var win = '<div id="lightbox-window">';
+				var end = '</div></div></div>';
+
+				$('body').append(win + bclo + back + lightbox + ifr + end);
+				$('#lightbox-window').hide();
+
+				if ($(this).data('videosite') == "youtube") {
+					var url = 'http://www.youtube.com/embed/' + $(this).data('videoid') + '?autoplay=1';
+				} else if($(this).data('videosite') == "vimeo") {
+					var url = 'http://player.vimeo.com/video/' + $(this).data('videoid') + '?autoplay=1';
+				}
+
+				$('#lightbox-window').fadeIn(100);
+				$('#video-embed').attr('src', url);
+
+				$('#background-close').click(function()
+				{
+					$('#lightbox-window').fadeOut($.simpleLightboxVideo.vars.delayAnimation, function()
+					{
+						$(this).remove();
+					});
+				});
+
+				return false;
+			});
+
+			$(document).keyup(function(e)
+			{
+				if (e.keyCode == 27)
+				{
+					$('#lightbox-window').fadeOut($.simpleLightboxVideo.vars.delayAnimation, function()
+					{
+						$(this).remove();
+					});
+				}
+			});
+
+			$(window).resize(function()
+			{
+				if (window.innerHeight > 540)
+				{
+					var margintop = (window.innerHeight - 540) / 2;
+				}
+				else
+				{
+					var margintop = 0;
+				}
+
+				$('.lightbox').css({
+					marginTop: margintop + 'px'
+				});
+			});
+
+			return false;
+		}
+	});
+})(jQuery);
+
+(function($)
+{
+	$.simpleLightboxVideo = function(options, video)
+	{
+		return $(video).simpleLightboxVideo();
+	}
+})(jQuery);
---
+++ styles/gallery filter/photos-filter.js
@@ -0,0 +1,36 @@
+$(window).load(function () {
+
+var button = 1;
+var button_class = "filter-current";
+var $container = $('#gallery-content');
+    
+$container.isotope({itemSelector : 'img'});
+
+
+function check_button(){
+	$('.filter').removeClass(button_class);
+	if(button==1){
+		$("#all").addClass(button_class);
+		}
+	if(button==2){
+		$("#group1").addClass(button_class);
+		}
+	if(button==3){
+		$("#group2").addClass(button_class);
+		}	
+	if(button==4){
+		$("#group3").addClass(button_class);
+		}	
+	if(button==5){
+		$("#group4").addClass(button_class);
+		}	
+}
+
+$("#all").click(function() { $container.isotope({ filter: '.all' }); button = 1; check_button(); });
+$("#group1").click(function() {  $container.isotope({ filter: '.group1' }); button = 2; check_button();  });
+$("#group2").click(function() {  $container.isotope({ filter: '.group2' }); button = 3; check_button();  });
+$("#group3").click(function() {  $container.isotope({ filter: '.group3' }); button = 4; check_button();  });
+$("#group4").click(function() {  $container.isotope({ filter: '.group4' }); button = 5; check_button();  });
+
+check_button();
+});
